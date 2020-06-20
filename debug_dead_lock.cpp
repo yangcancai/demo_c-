f3ed0556be44395ea184f87dab5baf48d9ff6467
Cam <710180334@qq.com>
2020-06-20 11:31:08 +0800

    修改代码让其更容易死锁

--- debug_dead_lock.cpp
+++ debug_dead_lock.cpp
@@ -5,11 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <iostream>
+#include <unistd.h>
+
 typedef struct thread_str{
     int rs;
 }thread_str;
-int g_tickets = 10;
-int g_num = 10;
+int g_tickets = 1000;
+int g_num = 1000;
 pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t g_mutex_num = PTHREAD_MUTEX_INITIALIZER;
 void sell_num(int thread){
@@ -25,7 +28,8 @@ void* thread_proc1(void* arg)
     while (1)
     {
         pthread_mutex_lock(&g_mutex);
-//        sell_num(1);
+        usleep(10);
+        sell_num(1);
         if (g_tickets > 0)
             printf("thread 1 sell tickets:%d\n", g_tickets--);
         else
@@ -51,6 +55,9 @@ void* thread_proc2(void* arg)
         else
         {
             pthread_mutex_unlock(&g_mutex);
+            if (g_num > 0)
+                printf("thread%d sell num:%d\n", 2, g_num--);
+            pthread_mutex_unlock(&g_mutex_num);
             break;
         }
         pthread_mutex_unlock(&g_mutex);
@@ -70,13 +77,10 @@ int main(int argc, char*argv[])
     thread_str t1,t2;
     pthread_create(&tid1, NULL, thread_proc1, &t1);
     pthread_create(&tid2, NULL, thread_proc2, &t2);
-
     pthread_join(tid1, &ret1);
     pthread_join(tid2, &ret2);
-
     printf("ret1:%d\n", static_cast<thread_str*>(ret1)->rs);
     printf("ret2:%d\n", static_cast<thread_str*>(ret2)->rs);
     getchar();
-
     return 0;
 }
