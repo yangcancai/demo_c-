2f45fcc01bce431bc45024e4d76183d000777af9
Cam <710180334@qq.com>
2020-07-04 12:25:16 +0000

    add pthread link & update submodules

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-cmake -S . -B cmake-build-debug  -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug && cd build && make clean && make all && ./debug_dead_lock
+git submodule update --init --recursive
+cmake -S . -B build  -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug && cd build && make clean && make all && ./debug_dead_lock
