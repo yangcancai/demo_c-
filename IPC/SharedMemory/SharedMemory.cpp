200220e75745f77469e68b141abddf9ff68f594c
Cam <710180334@qq.com>
2020-07-07 23:39:53 +0800

    do some test for interprocess communication

--- IPC/SharedMemory/SharedMemory.cpp
+++ IPC/SharedMemory/SharedMemory.cpp
@@ -3,15 +3,15 @@
 //
 
 #include <boost/interprocess/shared_memory_object.hpp>
+#include <boost/interprocess/mapped_region.hpp>
 #include <iostream>
 int main()
 {
     boost::interprocess::shared_memory_object shdmem(boost::interprocess::open_or_create,"Highscore", boost::interprocess::read_write);
     shdmem.truncate(1024);
-    std::cout << shdmem.get_name() << std::endl;
-    boost::interprocess::offset_t size;
-    if (shdmem.get_size(size)){
-        std::cout << size << std::endl;
-    }
+    boost::interprocess::mapped_region region(shdmem, boost::interprocess::read_write);
+    int* i = static_cast<int*>(region.get_address());
+    std::cout << *i << std::endl;
+    getchar();
     return 0;
 }
